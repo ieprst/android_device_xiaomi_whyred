a9500da59583f31606495c782f6ac6c4bb1f044d
ieprst <letrale@mail.ru>
2018-08-12 08:51:56 +0300

    whyred: Update to MIUI 8.8.9
     * Update build fingerprint
     * Update vendor security patch level
     * Update blobs

--- lineage.mk
+++ lineage.mk
@@ -41,5 +41,5 @@ TARGET_VENDOR_DEVICE_NAME := whyred
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=whyred PRODUCT_NAME=whyred
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 8.7.26 release-keys"
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/8.7.26:user/release-keys
+    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 8.8.9 release-keys"
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/8.8.9:user/release-keys
